45c0504d32dada0e632790a2a1091b210e6f1f75
sakai-code <76587090+sakai-code@users.noreply.github.com>
2022-01-22 23:01:23 +0900

    test

---
+++ myBlocks.cpp
@@ -0,0 +1,21 @@
+#include "pxt.h"
+
+
+using namespace pxt;
+
+
+namespace myBlocks
+{
+   
+  //%  
+    void hello()   
+    {  
+
+    uBit.display.scroll("HELLO WORLD!",50);
+
+
+   
+    }
+  
+  
+}
--- myBlocks.ts
+++ myBlocks.ts
@@ -1,1 +1,24 @@
 // ここにコードを追加します
+   
+// ここにコードを追加します
+   
+
+//% icon="\uf03e"
+namespace myBlocks{
+
+    /**
+     * display hello world 
+     */
+    //% help=myBlocks/hello
+    //% blockid=myblockhello
+    //% block shim=myBlocks::hello
+export function hello() {
+
+    basic.showString("HELLO WORLD");
+        
+    };
+
+
+}
+
+
